faa1883210f8d796a5521537843258c84d762c5a
Kunal Surya Shekhar <kunal2002surya@gmail.com>
2023-12-26 21:42:05 +0530

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+const app = express();
+// app.get("/", (req, res) => {
+//   res.send("Server is Ready");
+// });
+app.get("/api/jokes", (req, res) => {
+    const jokes = [{
+        id :1 ,
+        title : 'a joke ',
+        content : "this is a Joke"
+    },
+    {
+        id :2 ,
+        title : '2nd joke ',
+        content : "this is 2nd Joke"   
+    }
+]
+    res.send(jokes);
+  });
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Serve at http://localhost:${port}`);
+});
